7fa4c483b26d835b052fe9e07a9cd0446405c106
irpsv <i.rpsv@live.com>
2016-07-28 18:01:49 +0500

    add exception classes

---
+++ olof.juggernaut/lib/Exception/ComponentParameterException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jugger\Exception;
+
+/**
+ * исключение при ошибке параметров компонентов
+ * @author Ilya Rupasov <i.rpsv@live.com>
+ */
+class ComponentParameterException extends ComponentException
+{
+}
---
+++ olof.juggernaut/lib/Exception/SorterException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jugger\Exception;
+
+/**
+ * Description of SorterException
+ *
+ * @author Ilya Rupasov <i.rpsv@live.com>
+ */
+class SorterException extends \Exception
+{
+}
